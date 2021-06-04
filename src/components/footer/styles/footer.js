e600c3e4e9164b14db8183a5d8c5bfa923eef72f
Christina Harris <christinathedesigner@outlook.com>
2021-06-04 13:01:42 -0700

    completed footer

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -14,7 +14,7 @@ flex-direction: column;
 
 export const Row = styled.div`
 display: grid;
-grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
 grid-gap: 15px;
 
 @media (max-width 1000px) {
@@ -31,7 +31,7 @@ text-align:left;
 export const Link = styled.a`
 color:#757575;
 margin-bottom:20px;
-font-size: 13px;
+font-size: 14 px;
 text-decoration: none;
 `;
 
