0a16b15b0129c3dff2f5b3a194087cd5af56f982
Christina Harris <christinathedesigner@outlook.com>
2021-06-06 12:35:56 -0700

    finished accordion index file

---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Inner = styled.div``;
+export const Frame = styled.div``;
+export const Title = styled.p``;
+export const Item = styled.div``;
+export const Container = styled.div``;
+export const Header = styled.div``;
+export const Body = styled.div``;
