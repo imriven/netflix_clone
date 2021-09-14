b273ae4d3ddb1394284060f44ce69b02c9f78854
Christina Harris <christinathedesigner@outlook.com>
2021-09-14 13:02:12 -0700

    protected routes added to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import { Home, Browse, Signin, Signup} from "./pages"
+import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+
+export default function App() {
+  const user = {name: "Karl"};
+
+  return (
+    <Router>
+      <Switch>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        
+      >
+        <Signin />
+      </IsUserRedirect>
+
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        
+      >
+        <Signup />
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact>
+        <Home />
+        </IsUserRedirect>
+      </Switch>
+    </Router>
+  );
+}
+
+/*
+This is how we set up routes before protecting routes
 
 export default function App() {
   return (
     <Router>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
       <Route exact path={ROUTES.SIGN_IN}>
+        <IsUserRedirect user={user}
         <Signin />
       </Route>
       <Route exact path={ROUTES.SIGN_UP}>
         <Signup />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
       </Route>
       <Route exact path={ROUTES.HOME}>
         <Home />
@@ -21,3 +60,4 @@ export default function App() {
     </Router>
   );
 }
+*/
